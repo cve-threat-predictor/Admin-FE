440dbe51bcb3bb5898a5952d4b08eb242177adc0
yanng93 <yangheeji93@gmail.com>
2025-10-25 21:40:16 +0900

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+// app/page.tsx
+// 레포 루트에 있는 coming-soon.tsx를 불러옵니다.
+import ComingSoonPage from "../coming-soon";
+
+export default function Home() {
+  return <ComingSoonPage />;
+}
